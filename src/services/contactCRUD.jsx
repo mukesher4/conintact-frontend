e1869e8407af68428d8793cccdfbc4d8d7e98340
Mukesh R <115150217+mukesher4@users.noreply.github.com>
2025-01-31 22:48:29 +0530

    Update contactCRUD.jsx

--- src/services/contactCRUD.jsx
+++ src/services/contactCRUD.jsx
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie';
 
-const URL = "http://localhost:5001/api/contacts/";
+const URL = "conintact-backend.vercel.app/api/contacts/";
 
 const sessionToken = Cookies.get("sessionToken");
 
