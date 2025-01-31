8985678afe92eef9be1801514bcd43fe5941be02
Mukesh R <115150217+mukesher4@users.noreply.github.com>
2025-01-31 23:10:43 +0530

    Update useSessionCheck.jsx

--- src/services/useSessionCheck.jsx
+++ src/services/useSessionCheck.jsx
@@ -8,7 +8,7 @@ const useSessionCheck = (navigate, setSessionExpire) => {
 	useEffect(() => {
 		const interval = setInterval(async () => {
 		try {
-			const response = await fetch("conintact-backend.vercel.app/api/sessionStatus", {
+			const response = await fetch("https://conintact-backend.vercel.app/api/sessionStatus", {
 				method: "GET",
 				headers: {
 				Authorization: `Bearer ${sessionToken}`,
